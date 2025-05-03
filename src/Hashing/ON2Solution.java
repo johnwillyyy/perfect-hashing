f4674bda0b18943755481780ff3b408f03874b86
kero <kerosamy42004@gmail.com>
2025-05-03 18:21:27 +0300

    frist commit

---
+++ src/Hashing/ON2Solution.java
@@ -0,0 +1,28 @@
+package Hashing;
+
+public class ON2Solution implements PrefectHashing {
+    @Override
+    public boolean insert(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean search(String s) {
+        return false;
+    }
+
+    @Override
+    public int NumberOfRehashes() {
+        return 0;
+    }
+
+    @Override
+    public int SizeOfHashTable() {
+        return 0;
+    }
+}
